91482f481851ec41d538392c8f5d4f849c2779de
Darckauwu <57506546+Darckauwu@users.noreply.github.com>
2024-08-01 12:37:38 -0600

    Update automatizmo_SII.py
    Código para proyectos SII v1.0

--- automatizmo_SII.py
+++ automatizmo_SII.py
@@ -1,85 +1,96 @@
-from pymodbus.client import ModbusSerialClient as ModbusClient
-import time
+from pymodbus.client import ModbusSerialClient as ModbusClient #Libreria necesaria para utilizar modbus
+import time #Libreria necesaria para detener el código X segundos
 
-client = ModbusClient(
-	port = '/dev/rs485',
-	baudrate = 9600,
-	parity = 'N',
-	stopbits = 1,
-	bytesize = 8,
-	timeout = 1
+client = ModbusClient( #Se formula el cliente
+	port = '/dev/rs485', #Puerto a utilizar (rs485)
+	baudrate = 9600, #El baudage de la conexión (9600)
+	parity = 'N', #La paridad (None)
+	stopbits = 1, #El bit de parada (1)
+	bytesize = 8, #Tamaño de byte (8)
+	timeout = 1 #Timeout para la conexión (1)
 	)
 
-if not client.connect():
-	print("ERROR AL CONECTAR")
-	exit()
+if not client.connect(): #Sí al hacer la conexión hay un error, entra aquí
+	print("ERROR AL CONECTAR") #log
+	exit() #Cierra el script
 
-def apagarPorError():
-	print("##--## MOTOR APAGADO POR ERROR EN CONEXION ##--##")
-	escribirSalida(0,0,31)
+def apagarPorError(): #Funcion para apagar el motor si no se ha podido leer/escribir una variable
+	print("##--## MOTOR APAGADO POR ERROR EN CONEXION ##--##") #log
+	timeout = 0 #intentos
+	while True: #Mantiene el código corriendo permanentemente hasta que decida que hacer
+		escritura = client.write_coil(0,0,31) #Manda a apagar el motor
+		if not escritura.isError(): #Sí no hubo errores entra aquí
+			print("Direccion:",direccion," Valor:",escritura," Esclavo:",esclavo) #log
+			break #Rompe el while para seguir con el código
+		time.sleep(5) #Espera X segundos para volver a intentar a apagar el motor
+	#Este código se mantiene ejecutado hasta que se pueda apagar el motor. Sí no se apaga el motor, se quedará aquí para siempre.
 
-def escribirSalida(direccion,valor,esclavo):
-	lectura = client.write_coil(direccion,valor,esclavo)
-	print("Direccion:",direccion," Valor:",valor," Esclavo:",esclavo)
-	while lectura.isError():
-		lectura = client.write_coil(direccion,valor,esclavo)
-		timeout += 1
-		print("ERROR ",timeout," EN LA ESCRITURA")
-		if timeout == 10:
-			apagarPorError()
-			break
-		time.sleep(5)
+def leerDigital(direccion,esclavo): #funcion para leer digitales
+	timeout = 0 #intentos
+	while True: #Mantiene el código corriendo permanentemente hasta que decida que hacer
+		lectura = client.read_discrete_inputs(direccion,1,esclavo) #Lee la digital por medio de modbus
+		if not lectura.isError(): #Sí la lectura no tuvo errores entra aquí
+			print("Direccion:",direccion," Valor:",lectura," Esclavo:",esclavo) #log
+			return lectura.bits[0] #Regresa el valor de la digital
+		#Sí hubo error en la lectura continúa aquí el código
+		timeout += 1 #Se agrega un intento
+		print("ERROR ",timeout," EN LA LECTURA") #log
+		if timeout == 10: #Sí se cumplen 10 intentos entra aquí
+			apagarPorError() #Manda a apagar el motor por seguridad
+			return False #Regresa un valor falso (0) para salir de la función
+		time.sleep(5) #Espera X segundos para volver a intentar a leer la digital
 
-def leerDigital(direccion,esclavo):
-	timeout = 0
-	lectura = client.read_discrete_inputs(direccion,1,esclavo)
-	while lectura.isError():
-		lectura = client.read_discrete_inputs(direccion,1,esclavo)
-		timeout += 1
-		print("ERROR ",timeout," EN LA LECTURA")
-		if timeout == 10:
-			apagarPorError()
-			break
-		time.sleep(5)
-	print("Direccion:",direccion," Valor:",lectura," Esclavo:",esclavo)
-	return lectura.bits[0]
+def escribirSalida(direccion,valor,esclavo): #Funcion para encender/apagar una salida
+	timeout = 0 #intentos
+	while True: #Mantiene el código corriendo permanentemente hasta que decida que hacer
+		escritura = client.write_coil(direccion,valor,esclavo) #Escribe la salida por medio de modbus
+		if not escritura.isError(): #Sí la lectura no tuvo errores entra aquí
+			print("Direccion:",direccion," Valor:",escritura," Esclavo:",esclavo) #log
+			break #Rompe el while para salir de la función
+		#Sí hubo error en la lectura continúa aquí el código
+		timeout += 1 #Se agrega un intento
+		print("ERROR ",timeout," EN LA ESCRITURA") #log
+		if timeout == 10: #Sí se cumplen 10 intentos entra aquí
+			apagarPorError() #Manda a apagar el motor por seguridad
+			break #Rompe el while para salir de la función
+		time.sleep(5)#Espera X segundos para volver a intentar a leer la digital
 
-def leerSalida(direccion,esclavo):
-	lectura = read_coils(direccion,esclavo)
-	while lectura.isError():
-		lectura = read_coils(direccion,esclavo)
-		timeout += 1
-		print("ERROR ",timeout," EN LA LECTURA")
-		if timeout == 10:
-			apagarPorError()
-			break
-		time.sleep(5)
-	print("---",lectura.bits[0],"---")
-	return lectura.bits[0]
+def leerSalida(direccion,esclavo): #funcion para leer el estado de la salida al motor
+	timeout = 0 #intentos
+	while True: #Mantiene el código corriendo permanentemente hasta que decida que hacer
+		lectura = client.read_coils(direccion,1,esclavo) #Lee la salida por medio de modbus
+		if not lectura.isError(): #Sí la lectura no tuvo errores entra aquí
+			print("Direccion:",direccion," Valor:",lectura," Esclavo:",esclavo) #log
+			return lectura.bits[0] #Regresa el valor de la digital
+		#Sí hubo error en la lectura continúa aquí el código
+		timeout += 1 #Se agrega un intento
+		print("ERROR ",timeout," EN LA LECTURA") #log
+		if timeout == 10: #Sí se cumplen 10 intentos entra aquí
+			apagarPorError() #Manda a apagar el motor por seguridad
+			return False #Regresa un valor falso (0) para salir de la función
+		time.sleep(5) #Espera X segundos para volver a intentar a leer la digital
 
-def automatizmoMotor(DI1,DI2):
-	if not DI1 and not DI2:
-		print("--- Encender motor ---")
-		return True
-	elif DI1 and DI2:
-		print("--- Apagar motor ---")
-		return False
-	else:
-		print("--- Se queda igual ---")
-		return leerSalida(0,31)
+def automatizmoMotor(DI1,DI2): #funcion para decidir que hacer con el motor (encender/apagar)
+	if not DI1 and not DI2: #Sí los flotadores están abajo (0,0) entra aquí
+		print("--- Encender motor ---") #log
+		return True #Regresa el valor para encender el motor
+	elif DI1 and DI2: #Sí los flotadores están arriba (1,1) entra aquí
+		print("--- Apagar motor ---") #log
+		return False #Regresa el valor para apagar el motor
+	else: #Sí no se cumple lo anterior quiere decir que el agua está a la mitad (llenandose/vaciandose)
+		print("--- Se queda igual ---") #log
+		return leerSalida(0,31) #Regresa el valor actual del motor (no hay cambios en su estado)
 
+try: #Intenta hacer el código, esto con la finalidad de manejar los errores que pudieran ocurrir
+	while True: # Mantiene el código corriendo permanentemente
+		print("########## Empezando lectura ##########") #log
+		DI1 = leerDigital(0,32) #Lectura de la digital 1
+		DI2 = leerDigital(1,32) #Lectura de la digital 2
+		escribirSalida(0,automatizmoMotor(DI1,DI2),31) #Apaga o prende el motor con respecto a las digitales
+		print("########## Esperando 5 segundos ##########") #log
+		time.sleep(5) #Espera X segundos para hacer otra lectura
 
-
-try:
-	while True:
-		print("########## Empezando lectura ##########")
-		DI1 = leerDigital(0,32)
-		DI2 = leerDigital(1,32)
-		escribirSalida(0,automatizmoMotor(DI1,DI2),31)
-		print("########## Esperando 5 segundos ##########")
-		time.sleep(5)
-
-except Exception as e:
+except Exception as e: #Sí hay algún error en el código lo muestra en pantalla.
 	print(e)
-finally:
+finally: #Cierra la sesión con el puerto.
 	client.close()
